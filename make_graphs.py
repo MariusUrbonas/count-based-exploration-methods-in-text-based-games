3e0f67b793b036ad4b1bb0175bc32297d7e73e28
leonoverweel <l.overweel@gmail.com>
2019-03-14 20:45:27 +0000

    Add baseline plotting to make_graphs

--- make_graphs.py
+++ make_graphs.py
@@ -6,6 +6,40 @@ import pickle
 import sys
 
 
+NUM_EPOCHS = 500
+
+
+def plot_stats(stats_folder, quest_length, label, color):
+    stats_files = glob.glob('{}/*ql-{}*.pickle'.format(stats_folder, quest_length))
+
+    data_list = []
+    for stats_file in stats_files:
+        with open(stats_file, 'rb') as pickle_file:
+            data = pickle.load(pickle_file)
+            data_list.append([data[epoch]['steps'] for epoch in data])
+    
+    if len(data_list) == 0:
+        return
+
+    data_np = np.array(data_list)[:, :NUM_EPOCHS, :]
+    data_mean = np.mean(data_np, (0, 2))
+    data_std = np.std(data_np, (0, 2))
+
+    plt.fill_between(
+        np.arange(NUM_EPOCHS),
+        data_mean - data_std,
+        data_mean + data_std,
+        color=color,
+        alpha=0.15
+    )
+    plt.plot(
+        np.arange(NUM_EPOCHS), 
+        data_mean,
+        color=color,
+        label=label
+    )
+
+
 # Get parameters
 try:
     stats_folder_baseline = sys.argv[1]
@@ -15,52 +49,28 @@ except:
     print('Usage: pythonw make_graphs.py stats_folder_baseline stats_folder_comparison output_folder')
     exit()
 
-# Make output folder
-try:
-    # os.mkdir('figures/{}'.format(output_folder))
-    pass
-except:
-    print('Output folder already exists')
-    exit()
-
-num_epochs = 501
-
 # Make a separate graph for each quest length
 for i in range(5):
     quest_length = i + 1
 
     plt.figure()
     plt.ylim(0, 100)
-    plt.xlim(0, 500)
+    plt.xlim(0, NUM_EPOCHS)
     plt.title('Quest Length {}'.format(quest_length))
-
-    # Get stats files for different seeds
-    stats_files_comparison = glob.glob('{}/*ql-{}*.pickle'.format(stats_folder_comparison, quest_length))
-
-    comparison_data = []
-    for stats_file in stats_files_comparison:
-        with open(stats_file, 'rb') as pickle_file:
-            data = pickle.load(pickle_file)
-            comparison_data.append([data[epoch]['steps'] for epoch in data])
     
-    if len(comparison_data) == 0:
-        continue
-
-    comparison_data_np = np.array(comparison_data)
-    comparison_data_mean = np.mean(comparison_data_np, (0, 2))
-    comparison_data_std = np.std(comparison_data_np, (0, 2))
-
-    plt.fill_between(
-        np.arange(num_epochs),
-        comparison_data_mean - comparison_data_std,
-        comparison_data_mean + comparison_data_std,
-        color='#c4daff'
+    plot_stats(
+        stats_folder_baseline, 
+        quest_length, 
+        stats_folder_baseline.split('/')[-1], 
+        '#ef8a62'
     )
-    plt.plot(
-        np.arange(num_epochs), 
-        comparison_data_mean,
-        color='#216eef'
+
+    plot_stats(
+        stats_folder_comparison, 
+        quest_length, 
+        stats_folder_comparison.split('/')[-1], 
+        '#67a9cf'
     )
-    
-    plt.show()
-    # plt.savefig('figures/{}/quest-length-{}.pdf'.format(output_folder, quest_length), bbox_inches='tight')
+
+    # plt.show()
+    plt.savefig('figures/{}/quest-length-{}.pdf'.format(output_folder, quest_length), bbox_inches='tight')
