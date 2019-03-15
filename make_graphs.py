2a9db4d722d6ca3cd2baeb94c2b78d7a7f488d2d
leonoverweel <l.overweel@gmail.com>
2019-03-15 12:56:13 +0000

    Implement subplots in make_graphs

--- make_graphs.py
+++ make_graphs.py
@@ -15,7 +15,7 @@ STATS_FOLDERS = {
 }
 
 
-def plot_stats(stats_folder, quest_length, label, color):
+def plot_stats(axis, stats_folder, quest_length, label, color):
     stats_files = glob.glob('experiments/{}/stats/*ql-{}*.pickle'.format(stats_folder, quest_length))
 
     data_list = []
@@ -31,14 +31,14 @@ def plot_stats(stats_folder, quest_length, label, color):
     data_mean = np.mean(data_np, (0, 2))
     data_std = np.std(data_np, (0, 2))
 
-    plt.fill_between(
+    axis.fill_between(
         np.arange(NUM_EPOCHS),
         data_mean - data_std,
         data_mean + data_std,
         color=color,
-        alpha=0.15
+        alpha=0.1
     )
-    plt.plot(
+    axis.plot(
         np.arange(NUM_EPOCHS), 
         data_mean,
         color=color,
@@ -48,24 +48,32 @@ def plot_stats(stats_folder, quest_length, label, color):
 
 # Get parameters
 try:
-    output_folder = sys.argv[1]
+    output_name = sys.argv[1]
 except:
     print('Usage: pythonw make_graphs.py output_folder')
     exit()
 
-# Make a separate graph for each quest length
-for i in range(NUM_GRAPHS):
-    quest_length = i + 1
+fig, axes = plt.subplots(NUM_GRAPHS, 1, figsize=(6, 4 * NUM_GRAPHS), sharex=True, sharey=True)
 
-    plt.figure()
-    plt.ylim(0, 100)
-    plt.xlim(0, NUM_EPOCHS)
-    plt.title('Quest Length {}'.format(quest_length))
-    
+# Make a separate subplot for each quest length
+for axis, quest_length in zip(axes, range(1, NUM_GRAPHS + 1)):
+
     for stats_folder in STATS_FOLDERS:
         label, color = STATS_FOLDERS[stats_folder]
-        plot_stats(stats_folder, quest_length, label, color)
+        plot_stats(axis, stats_folder, quest_length, label, color)
+    # Set title and legend
+    axis.set_title('Quest Length {}'.format(quest_length))
+    if quest_length == 1:
+        axis.legend()
 
-    # plt.show()
-    plt.legend()
-    plt.savefig('figures/{}/quest-length-{}.pdf'.format(output_folder, quest_length), bbox_inches='tight')
+    # Set axis limits
+    axis.set_ylim(0, 100)
+    axis.set_xlim(0, NUM_EPOCHS)
+
+    # Set axis labels
+    axis.set_ylabel('Steps')
+    if quest_length == NUM_GRAPHS:
+        axis.set_xlabel('Epoch')
+    
+
+plt.savefig('figures/{}.pdf'.format(output_name, quest_length), bbox_inches='tight')
