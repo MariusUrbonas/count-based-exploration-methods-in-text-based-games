dbc01ebb64620576c5a982f6b52bd19877be5b96
leonoverweel <l.overweel@gmail.com>
2019-03-19 11:52:14 +0000

    Fix folder for martin's graphs

--- make_graphs.py
+++ make_graphs.py
@@ -10,8 +10,8 @@ NUM_EPOCHS = 400
 NUM_GRAPHS = 4
 STATS_FOLDERS = {  # 'folder': ('label', '#hexcolor')
     'banana_baseline': ('DQN (Baseline)', '#1b9e77'),
-    'obs-count-rep-motivation-false-v2': ('DQN-UCB-SA', '#d95f02'),
-    'obs-count-rep-motivation-true-v2': ('DQN-MBIE-EB', '#7570b3'),
+    'dqn-ubc-sa': ('DQN-UCB-SA', '#d95f02'),
+    'dqn-mbie-eb': ('DQN-MBIE-EB', '#7570b3'),
     'banana_cumulative': ('DQN-S+', '#e7298a'),
     'banana_episodic': ('DQN-S++', '#a6761d'),
     'cool_agent': ('DQN-KM++', '#e6ab02')
