cb4d78964e3e14d3563fb0f45154440c590b36a3
leonoverweel <l.overweel@gmail.com>
2019-03-20 23:17:25 +0000

    Add error bars

--- make_bar_chart.py
+++ make_bar_chart.py
@@ -4,6 +4,7 @@ import os
 import pickle
 import plotly.offline as py
 import plotly.graph_objs as go
+import plotly.io as pio
 import sys
 
 
@@ -23,12 +24,11 @@ STATS_FOLDERS = {  # 'folder': ('label', '#hexcolor')
 try:
     output_name = sys.argv[1]
 except:
-    print('Usage: pythonw make_graphs.py output_folder')
+    print('Usage: pythonw make_graphs.py output_name')
     exit()
 
+# Create trace for each model
 traces = []
-
-# Plot each model
 for index, stats_folder in enumerate(STATS_FOLDERS):
     
     # Get data for each quest length
@@ -42,39 +42,72 @@ for index, stats_folder in enumerate(STATS_FOLDERS):
                 if 'obs_set' in data:
                     del(data['obs_set'])
                 data_lists[-1].append([data[epoch]['scores'] for epoch in data][:NUM_EPOCHS])
-
+    
     data_np = np.array(data_lists)
-    data_means = np.mean(data_np, (1, 2, 3))
-    data_means_50 = np.mean(data_np[:, :, -50:, :], (1, 2, 3))
-
     label, color = STATS_FOLDERS[stats_folder]
 
+    # Mean of all epochs
     traces.append(go.Bar(
-        x=['QL {}'.format(ql + 1) for ql in range(NUM_QUEST_LENGTHS)],
-        y=data_means,
+        x=['Quest Length {}'.format(ql + 1) for ql in range(NUM_QUEST_LENGTHS)],
+        y=np.mean(data_np, (1, 2, 3)),
         name=label,
         marker=dict(
             color=color
+        ),
+        error_y=dict(
+            type='data',
+            array=np.std(data_np, (1, 3)).mean((1,)),
+            visible=True,
+            thickness=0.3,
+            width=2
         )
     ))
 
+    # Mean of last 50 epochs
     traces.append(go.Bar(
-        x=['QL {}'.format(ql + 1) for ql in range(NUM_QUEST_LENGTHS)],
-        y=data_means_50,
+        x=['Quest Length {}'.format(ql + 1) for ql in range(NUM_QUEST_LENGTHS)],
+        y=np.mean(data_np[:, :, -50:, :], (1, 2, 3)),
         marker=dict(
             color=color,
             opacity=0.3
         ),
-        showlegend=False
+        error_y=dict(
+            type='data',
+            array=np.std(data_np[:, :, -50:, :], (1, 3)).mean((1,)),
+            visible=True,
+            thickness=0.3,
+            width=2
+        ),
+        showlegend=False,
     ))
 
 layout = go.Layout(
     barmode='group',
-    bargroupgap=0.1,
+    bargroupgap=0.15,
     legend=dict(
-        x=0.85,
-        y=1,
+        orientation='h',
+        font=dict(
+            family='sans-serif',
+            size=8
+        ),
+    ),
+    xaxis=dict(
+        tickfont=dict(
+            family='sans-serif',
+            size=10
+        ),
+    ),
+    yaxis=dict(
+        title='Mean Score',
+        titlefont=dict(
+            family='sans-serif',
+            size=10
+        ),
+        tickfont=dict(
+            family='sans-serif',
+            size=10
+        ),
     )
 )
 fig = go.Figure(data=traces, layout=layout)
-py.plot(fig, filename='figures/grouped-bar.html')
+pio.write_image(fig, 'figures/{}.pdf'.format(output_name))
